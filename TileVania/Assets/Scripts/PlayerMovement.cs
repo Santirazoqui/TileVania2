47af84e477e6fef1ca61715e52f189f9ed7b2577
Santirazoqui <71331155+Santirazoqui@users.noreply.github.com>
2023-10-25 00:53:23 -0300

    Jump if IsTouchingLayers

--- TileVania/Assets/Scripts/PlayerMovement.cs
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -10,11 +10,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float runSpeed = 10f;
     [SerializeField] float jumpSpeed = 10f;
     Animator myAnimator;
+    CapsuleCollider2D myCapsuleCollider;
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
@@ -47,6 +49,7 @@ public class PlayerMovement : MonoBehaviour
 
     void OnJump(InputValue value)
     {
+        if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) return;
         if(value.isPressed)
         {
             myRigidbody.velocity += new Vector2(0f, jumpSpeed);
